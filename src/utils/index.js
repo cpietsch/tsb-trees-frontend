27c4eacfd0fe9adf82c04bdbedcd5697ccddda96
Christopher Pietsch <cpietsch@gmail.com>
2020-06-15 15:43:44 +0200

    improved loop for createGeojson
    save some milliseconds on parsing

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,8 +6,8 @@ export function createGeojson(data) {
     type: 'FeatureCollection',
     features: [],
   };
-
-  data.forEach(tree => {
+  for (var i = data.length - 1; i >= 0; i--) {
+    const tree = data[i];
     const feature = {
       type: 'Feature',
       geometry: {
@@ -27,7 +27,7 @@ export function createGeojson(data) {
       },
     };
     geojson.features.push(feature);
-  });
+  }
 
   return geojson;
 }
